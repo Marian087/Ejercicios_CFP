28e611f267b2873c379445c9d92468870d0611bf
Marian087 <rodriguezflormarian@gmail.com>
2024-03-27 20:30:10 -0300

    Ej01

---
+++ Ej01/Ej01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ej01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            string edad;
+            int edadNumerica;
+
+            Console.WriteLine("Por favor ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Por favor ingrese su apellido: ");
+            apellido = Console.ReadLine();
+
+            Console.WriteLine("Por favor ingrese su edad: ");
+            edad = Console.ReadLine();
+
+            edadNumerica = int.Parse(edad); 
+           
+            Console.WriteLine($"Muchas gracias {nombre} {apellido}, su edad es {edadNumerica}");
+
+
+        }
+    }
+}
